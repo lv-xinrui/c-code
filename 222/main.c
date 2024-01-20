09c9f4eba6d8870a71faadc0d8eb56346b56da28
lv-xinrui <2298491249@qq.com>
2024-01-20 19:40:05 +0800

    Initial Commit

---
+++ 222/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  222
+//
+//  Created by 我爱吃糖 on 2023/10/1.
+//
+
+
+
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if (a!=0,b!=0)
+
+    {
+        printf("输入:%d %d\n",a,b);
+        printf("输出:%d %d\n", a / b,a % b);
+
+    }
+    return 0;
+}
