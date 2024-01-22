c982860958a0df95c1f3ffa9acce4a87e6da5852
lv-xinrui <156651182+lv-xinrui@users.noreply.github.com>
2024-01-22 15:44:02 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,30 +1,22 @@
 //
 //  main.c
-//  百钱买百鸡
-//鸡翁一值钱五，鸡母一值钱三，鸡雏三值钱一。百钱买百鸡，问鸡翁、母、雏各几何?(我国古代算术一-张丘建《算经》)含义:
-//1只公鸡5元钱，1只母鸡3元钱，3只小鸡1元钱，现100元钱买100只鸡，则公鸡、母鸡、小鸡应各买多少只?
-//公鸡5元/只    母鸡3元/只  小鸡 1/3 元/只
-//  Created by 我爱吃糖 on 2024/1/21.
+//  打印1～100间奇数
+//
+//  Created by 我爱吃糖 on 2024/1/22.
 //
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int cock = 0, hen = 0, chick = 0;
-    for (cock = 0; cock <= 100; cock++)
+    int i=1;
+    for (i=1; i<=100; i++)
     {
-        for (hen = 0; hen <= 100; hen++)
+        if (i%2==1)
         {
-            for (chick = 0; chick <= 100; chick++)
-            {
-                if ((cock + hen + chick == 100) && (5 * cock + 3 * hen + chick / 3 == 100)&&(chick%3==0))
-                {
-                    printf("cock:%d hen:%d chick:%d\n",cock,hen,chick);
-                }
-            }
+            printf("%d  ",i);
         }
     }
-
-
+    return 0;
 }
+
