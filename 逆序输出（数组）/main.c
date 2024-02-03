c40d32ede16446cbd3803e8f034361517a9dbb57
lv-xinrui <156651182+lv-xinrui@users.noreply.github.com>
2024-02-03 19:23:10 +0800

    Add files via upload

---
+++ 逆序输出（数组）/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  逆序输出（数组）
+//
+//  Created by 我爱吃糖 on 2024/2/3.
+//
+
+#include<stdio.h>
+int main()
+{
+    int arr[10] = {0};
+    for(int i = 9;i>=0;i--)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i = 0;i<10;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
